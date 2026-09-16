aa096a0ad787cc87f9ab8454ed528bcdc6512384
chinhan142 <buiphamchinhan@gmail.com>
2026-09-16 22:24:06 +0700

    Finish register function for auth module

--- src/module/auth/dto/register.dto.ts
+++ src/module/auth/dto/register.dto.ts
@@ -1,1 +1,35 @@
-export class RegisterDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStrongPassword()
+  password: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  departmentId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  jobTitleId: number;
+}
